fa3d8e9536ec3c82f2c9c5aa47248a6208862da1
newalina <157907056+newalina@users.noreply.github.com>
2025-12-07 21:04:40 -0800

    image

--- lib/types.ts
+++ lib/types.ts
@@ -1,9 +1,19 @@
 export interface Product {
   id: string;
   name: string;
+  subtitle: string;
   price: number;
-  image: string;
   description: string;
+  images: ProductImage[];
+  design: string[];
+  materials: string[];
+  make: string[];
+}
+
+export interface ProductImage {
+  id: string;
+  src: string;
+  alt: string;
 }
 
 export type View = "garden" | "grid";
