d49d56c463d41aeb1f07ccd56af0f97fd1b728a9
newalina <alina_kim@alumni.brown.edu>
2025-12-07 22:38:15 -0800

    products (#2)
    * product
    
    * view
    
    * image
    
    * data
    
    * get by id

---
+++ lib/types.ts
@@ -0,0 +1,26 @@
+export type View = "garden" | "grid";
+
+export type ProductImageType =
+  | "main"
+  | "carousel"
+  | "front"
+  | "back"
+  | "detail";
+
+export interface ProductImage {
+  src: string;
+  alt: string;
+  type: ProductImageType;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  subtitle: string;
+  price: number;
+  description: string;
+  design: string[];
+  materials: string[];
+  make: string[];
+  images: ProductImage[];
+}
